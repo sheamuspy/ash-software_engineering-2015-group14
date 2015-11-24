318f44a1e449dc0ef4c4589fd1a4e91ef6d300dc
sheamuspy <sheamuspy@gmail.com>
2015-11-24 23:58:11 +0000

    Change user interface for login page

--- public/login.php
+++ public/login.php
@@ -7,7 +7,7 @@ if (isset($_REQUEST['loginUsername'])) {
 	$obj = new users();
 	$row = $obj->user_password_validation($u_name,$p_word);
 	if (!$row) {
-		echo "wrong credentials";
+		echo "<div class='center'>wrong credentials</div>";
 	} else {
 		$_SESSION['USER_ID'] = $row['user_id'];
 		$_SESSION['USERNAME'] = $row['user_name'];
@@ -23,31 +23,39 @@ if (isset($_REQUEST['loginUsername'])) {
     <script src="jquery-2.1.3.js"></script>
     <script src="js/login.js"></script>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/materialize.min.css" />
 </head>
 
 <body>
-    <form method="POST" action="login.php">
-        <table align="center">
-            <tr>
-                <td>Username</td>
-                <td>
-                    <input type="text" placeholder="Username" name="loginUsername" required>
-                </td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td>
-                    <input type="password" placeholder="Password" name="loginPassword" required>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>
-                    <button type="submit">Login</button>
-                </td>
-            </tr>
-        </table>
-    </form>
+    <div class="row">
+        <div class="col s4 offset-s4">
+            <form method="POST" action="login.php" class="card-panel">
+                <div class="row center">
+                    <div class="col s4 offset-s1 valign-wrapper">
+                        <h5 class="valign right-align">Username</h5>
+                    </div>
+                    <div class="col s6">
+                        <input type="text" placeholder="Username" name="loginUsername" required>
+                    </div>
+                </div>
+                <div class="row center">
+                    <div class="col s4 offset-s1 valign-wrapper">
+                        <h5 class="valign right-align">Password</h5>
+                    </div>
+                    <div class="col s6">
+                        <input type="password" placeholder="Password" name="loginPassword" required>
+                    </div>
+                </div>
+                <div class="row center">
+                    <button class="btn waves-effect waves-light" type="submit">Login</button>
+                </div>
+
+
+
+            </form>
+        </div>
+    </div>
+
 </body>
 
 </html>
