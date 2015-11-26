3d5b9e6346574881b4da67e93945da9e61cda309
Ralph Quaye <quayeralph@gmail.com>
2015-11-26 18:31:29 +0000

    if/else block statements edited according to Zend framework

--- public/search_equipment.php
+++ public/search_equipment.php
@@ -13,32 +13,32 @@
 		$database = "webtech";
 
 		$link = mysql_connect($server, $username, $password);
-		if (!$link) {
+		if (! $link) {
 			echo "Error connecting to server ::".mysql_error();
 			exit();
 		}
-		if (!mysql_select_db($database, $link)) {
+		if (! mysql_select_db($database, $link)) {
 			echo "error connecting to database ::".mysql_error();
 			exit();
 		}
 
 		$searchText = "";
 		if (isset($_REQUEST['st'])) {
-			$searchText=$_REQUEST['st'];
+			$searchText = $_REQUEST['st'];
 		}
 		echo "<form action='searchpage.php' method='GET'>"
 			."<input type='text' name='st' value='$searchText'>"
 			."<input type='submit' value='search'>"
 			. "</form>";
 
-			$query = "Select product_id, product_name, price From products where product_name like  '%$searchText%'";
-			$result= mysql_query($query);
+			$query = "Select product_id, product_name, price From products where product_name like '%$searchText%'";
+			$result = mysql_query($query);
 		echo	"<table border = '1'>";
 		echo	"<tr>";
 		echo	"<td>Product Name</td><td>Price</td>";
 		echo	"</tr>";
 		echo "<ol>";
-		while ($row=mysql_fetch_assoc($result)) {
+		while ($row = mysql_fetch_assoc($result)) {
 				echo "<tr><td><li><a href = 'viewpage.php?pid=".$row['product_id']."'>".$row['product_name']."</a></td>";
 				//echo " ";
 				echo "<td><a href = 'viewpage.php?pid=".$row['product_id']."'>".$row['price']."</a></td>";
