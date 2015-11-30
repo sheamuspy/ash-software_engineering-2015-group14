8366fb9457b49f74e2029a7cf30ef7728ea68cb6
Ralph Quaye <quayeralph@gmail.com>
2015-11-30 08:59:01 +0000

    entire code modified to fit new user interface and architecture model

--- public/search_equipment.php
+++ public/search_equipment.php
@@ -1,42 +1,109 @@
-/**
-* A method to search equipment in the database
-*
-* The file contains the sql statements to search for an equipment in the inventory database
-*
-* LICENSE:
-*
-* @Category Zend
-* @Package
-* @Copyright
-* @license
-* @version 1.0.1
-*/
+<?php
+	session_start();
+	if(!isset($_SESSION['USERNAME'])){
+		header("location:login.php");
+	}
 
-<html>
+?>
+    <!DOCTYPE html>
+    <html lang="en" style="padding-right: 0px;">
+
     <head>
-        <meta charset="UTF-8">
-		<title></title>
-	</head>
-	<body>
-		<a href="add_equipment.php">Click to Add Product</a>
-		<?php
-    //connect to the inventory databse
-		$server = "localhost";
-		$username="root";
-		$password="";
-		$database = "engineering_inventory";
+        <title>Equipment</title>
+        <link rel="stylesheet" href="css/materialize.min.css">
+        <link rel="stylesheet" href="css/style.css">
 
-		$link = mysql_connect($server, $username, $password);
-		if (! $link) {
-			echo "Error connecting to server ::".mysql_error();
-			exit();
-		}
-		if (! mysql_select_db($database, $link)) {
-			echo "error connecting to database ::".mysql_error();
-			exit();
-		}
+        <script>
+            var userId = <?php echo $_SESSION['USER_ID']; ?>;
+        </script>
 
-		$searchText = "";
+    </head>
+
+    <body>
+        <!-- This is the page header -->
+        <header>
+
+            <nav class="top-nav" id="pageheader">
+                <div class="container">
+                    <div class="nav-wrapper">
+                        <a class="page-title">Ashesi Engineering Inventory</a>
+                    </div>
+                </div>
+            </nav>
+
+            <ul id="mainnav" class="side-nav fixed " style="width: 240px;">
+                <li id="logo">
+    <img class="responsive-img circle center" src="images/logo.png">
+        </li>
+                <li>
+                    <a href="#">
+                        <?php echo $_SESSION['USERNAME']?>
+                            logged in</a>
+                </li>
+                <li>
+                    <a href="index.php">
+                        <div >Home</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="equipment_page.php">
+                        <div><b>Equipment</b></div>
+                    </a>
+                </li>
+                <li>
+                    <a href="labpage.php">
+                        <div>Labs</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="suppliers_page.php">
+                        <div>Supplier</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="history.php">
+                        <div>History</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="logout.php">
+                        <div>Logout</div>
+                    </a>
+                </li>
+            </ul>
+
+        </header>
+
+        <!-- This is the main section of the page -->
+        <main>
+<!--            <div class="container">-->
+                <div class="row">
+                    <div class="col s12 m9 l12">
+
+
+                        <div id="content" class="card-panel grey lighten-2">
+                            <div id="divPageMenu">
+                                <div style="float:left">
+                                    <span id="change" class="menuitem" onclick="loadAddEquipmentForm()">Add Equipment</span>
+                                    <span class="menuitem" id="edit" onclick="loadEditEquipmentForm()" hidden="true">Edit</span>
+                                    <span class="menuitem" onclick="deleteEquip()" id="deleteE" hidden="true">Delete</span>
+                                    <span class="menuitem" id="exit" onclick="exitView()" hidden="true">Exit</span>
+                                </div>
+                                <div align="right">
+                                    <input type="text" placeholder="Search" id="txtSearch" />
+                                    <span id="search" class="menuitem" onclick="search()">search</span>
+                                </div>
+                            </div>
+                            <div id="divStatus" class="status">
+                                status message
+                            </div>
+                            <div id="divContent">
+                                <div id="contentSpace"></div>
+                                <table id="tableExample" class="reportTable bordered" width="100%">
+                                    <?php
+
+include_once("../application/models/equipment.php");
+$searchText = "";
 		if (isset($_REQUEST['st'])) {
 			$searchText = $_REQUEST['st'];
 		}
@@ -45,27 +112,47 @@
 			."<input type='text' name='st' value='$searchText'>"
 			."<input type='submit' value='search'>"
 			. "</form>";
-    //sql statement to query the database
-			$query = "Select equipment_id, equipment_name, lab_id, date_purchased From webtech_project_equipment where equipment_name like '%$searchText%'";
 
-			$result = mysql_query($query);
-		echo	"<table border = '1'>";
-		echo	"<tr>";
-		echo	"<td>Equipment Name</td><td>Equipment ID</td><td>Date of Purchase</td><td>Lab ID</td>";
-		echo	"</tr>";
-		echo "<ol>";
-		while ($row= mysql_fetch_assoc($result)) {
-                echo "<tr><td><li><a href = 'view.php?pid=".$row['equipment_id']."'>".$row['equipment_name']."</a></td>";
-				echo "<td><a href = 'view.php?pid=".$row['equipment_id']."'>".$row['equipment_id']."</a></td>";
-                echo "<td><a href = 'view.php?pid=".$row['equipment_id']."'>".$row['date_purchased']."</a></td>";
-                echo "<td><a href = 'view.php?pid=".$row['equipment_id']."'>".$row['lab_id']."</a></td>";
-				echo "<td><a href = 'edit_equipment.php?pid=".$row['equipment_id']."'>".'[edit]'."</a></td>";
-				//echo " ";
-				echo "<td><a href = 'delete_equipment.php?pid=".$row['equipment_id']."'>".'[delete]'."</a></li></td></tr>";
-		}
-		echo "</ol>";
-		echo "</table>";
-		mysql_close($link);
-		?>
-	</body>
-</html>
+$obj= new equipment();
+$obj->search_equipment($searchText);
+//$lab= new equipment();
+	echo"<tr class='header'>
+		<td data-field='serial_number'>Serial Number</td>
+		<td data-field='name'>Equipment Name</td>
+		<td data-field='name'>Lab</td>
+		<td data-field='date'>Date Purchased</td>
+	    </tr>";
+
+    $count=0;
+	while ($row=$obj->fetch()) {
+        if ($count==0) {
+            $color = 'row1';
+            $count=1;
+        }
+        else {
+            $color = 'row2';
+            $count=0;
+        }
+		echo "<tr onclick='loadViewEquip($row[equipment_id])' class=$color style='cursor:pointer'><td>$row[serial_number]</td>";
+		echo "<td>$row[equipment_name]</td>";
+		echo "<td>$row[lab_id]</td>";
+		echo "<td>$row[date_purchased]</td></tr>";
+	}
+
+?>
+
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        </main>
+        <footer></footer>
+
+        <script src="jquery-2.1.3.js"></script>
+        <script src="js/equipment_page.js"></script>
+        <script src="js/materialize.min.js"></script>
+    </body>
+
+    </html>
