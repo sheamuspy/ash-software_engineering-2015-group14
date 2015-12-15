1084087650bdadf53d13235f5457165a912aec80
sheamuspy <sheamuspy@gmail.com>
2015-12-15 02:04:31 +0000

    Test for getting the checkout details written.

---
+++ tests/CheckoutTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class CheckoutTest extends PHPUnit_Framework_TestCase{
+
+    /**
+     * @uses /application/models/equipment An equipment class
+     */
+    public function testGetCheckoutDetails() {
+        include '../application/models/checkout.php';
+        $obj = new checkout();
+        $this -> assertTrue($obj->getCheckoutDetails());
+    }
+}
