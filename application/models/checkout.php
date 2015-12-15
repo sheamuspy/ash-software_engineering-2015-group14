049ef852a430b04b09acf6d83cc2203d67cb427e
sheamuspy <sheamuspy@gmail.com>
2015-12-15 03:03:05 +0000

    Fixes

--- application/models/checkout.php
+++ application/models/checkout.php
@@ -1,20 +1,20 @@
 <?php
-	include("adb.php");
+    include("adb.php");
 
-	class checkout extends adb{
+    class checkout extends adb{
 
-		function checkout(){}
+        function checkout(){}
 
-        function getCheckoutDetails(){
-            $str_query = "SELECT * FROM webtech_project_checkout
-                            INNER JOIN webtech_project_users ON
-							webtech_project_users.user_id = webtech_project_checkout.USER_ID INNER JOIN
-							webtech_project_equipment ON
-							webtech_project_equipment.equipment_id = webtech_project_checkout.EQUIPMENT_ID";
+        function get_checkout_details(){
+        $str_query = "SELECT * FROM webtech_project_checkout
+                        INNER JOIN webtech_project_users ON
+                        webtech_project_users.user_id = webtech_project_checkout.USER_ID INNER JOIN
+                        webtech_project_equipment ON
+                        webtech_project_equipment.equipment_id = webtech_project_checkout.EQUIPMENT_ID";
 
-            return $this->query($str_query);
-        }
+        return $this->query($str_query);
+    }
 
-	}
+    }
 
 ?>
