fc316b8cbfa85bfbcff0616fbddbeb336e313d7f
sheamuspy <sheamuspy@gmail.com>
2015-12-15 02:01:15 +0000

    Method to get checkout details added

--- application/models/checkout.php
+++ application/models/checkout.php
@@ -5,6 +5,16 @@
 
 		function checkout(){}
 
+        function getCheckoutDetails(){
+            $str_query = "SELECT * FROM webtech_project_checkout
+                            INNER JOIN webtech_project_users ON
+							webtech_project_users.user_id = webtech_project_checkout.USER_ID INNER JOIN
+							webtech_project_equipment ON
+							webtech_project_equipment.equipment_id = webtech_project_checkout.EQUIPMENT_ID";
+
+            return $this->query($str_query);
+        }
+
 	}
 
 ?>
